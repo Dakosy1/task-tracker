d3dbe1cd6401de12f7861463daf3e442534d3a32
Dakosy1 <ndauletkhan2004@mail.ru>
2026-05-22 12:36:54 +0500

    add command saves task to tasks.json

--- task-cli.js
+++ task-cli.js
@@ -10,7 +10,18 @@ console.log('Argument:', argument)
 if (command === 'add') {
     const data = fs.readFileSync('tasks.json', 'utf-8')
     const tasks = JSON.parse(data)
+    const newTask = {
+        id: tasks.length + 1,
+        description: argument,
+        status: 'todo',
+        createdAt: new Date().toISOString()
+    }
+    
+    tasks.push(newTask)
+    fs.writeFileSync('tasks.json', JSON.stringify(tasks, null, 2))
+    console.log(`Task added successfully (ID: ${newTask.id})`)    
     console.log(tasks)
 } else {
     console.log('Unknown command')
 }
+
